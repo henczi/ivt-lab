ffbdf319606a423a822aa94fabaaffb4b5858405
Henczidai Bálint <henczidaibalint@gmail.com>
2018-05-04 00:10:20 -0700

    fix

--- src/main/java/hu/bme/mit/spaceship/GT4500.java
+++ src/main/java/hu/bme/mit/spaceship/GT4500.java
@@ -38,7 +38,7 @@ public class GT4500 implements SpaceShip {
 
     boolean firingSuccess = false;
 
-      if(firingMode == SINGLE){
+      if (firingMode == SINGLE) {
         if (wasPrimaryFiredLast) {
           // try to fire the secondary first
           if (! secondaryTorpedoStore.isEmpty()) {
